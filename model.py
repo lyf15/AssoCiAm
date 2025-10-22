8db597f7a1779330b54dcaf5ebfc67487424c2c4
lyf15 <liuyf278@mail2.sysu.edu.cn>
2025-10-22 15:29:43 +0800

    Upload model.py

---
+++ model.py
@@ -0,0 +1,10 @@
+def load():
+    # Load your model and move it to the appropriate device (e.g., GPU or CPU).
+    # The loaded model should be defined as a global variable so that it can be
+    # accessed by the `response` function.
+    ...
+
+def get_response(img_path, question):
+    # Use your model to generate an answer based on the given image (img_path)
+    # and prompt. The returned answer must be a string such as "A", "B", "C", ...
+    ...    
